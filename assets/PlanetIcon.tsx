67f7fd59e67173ff3304f5c6d62d4c0aa185ea47
Rekun Ivan <vana.rekun@mail.ru>
2024-02-14 17:05:41 +0400

    setup apollo client, tabNavigation; styled tabNavigation

---
+++ assets/PlanetIcon.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Svg, {G, Path, Defs, ClipPath, SvgProps} from 'react-native-svg';
+
+type PlanetIconProps = SvgProps & {
+  color: string;
+};
+
+export const PlanetIcon = ({color, ...props}: PlanetIconProps) => {
+  return (
+    <Svg width={29} height={28} viewBox="0 0 29 28" fill="none">
+      <G
+        clipPath="url(#clip0_3217_1122)"
+        stroke={color}
+        strokeWidth={2}
+        strokeMiterlimit={10}
+        {...props}>
+        <Path d="M22.424 15.816c3.066 2.46 4.771 4.635 4.183 5.653-.93 1.607-7.228-.285-14.068-4.226S.91 8.804 1.837 7.198c.582-1.004 3.26-.642 6.857.734" />
+        <Path d="M14.222 22.666a8.333 8.333 0 100-16.666 8.333 8.333 0 000 16.666z" />
+      </G>
+      <Defs>
+        <ClipPath id="clip0_3217_1122">
+          <Path fill={color} transform="translate(.722)" d="M0 0H28V28H0z" />
+        </ClipPath>
+      </Defs>
+    </Svg>
+  );
+};
---
+++ constants/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  accent: {indigo: '#5856D6'},
+  grayBase: {
+    gray1: '#8E8E93',
+    gray2: '#AEAEB2',
+    gray3: '#C7C7CC',
+    gray4: '#D1D1D6',
+    gray5: '#E5E5EA',
+    gray6: '#F2F2F7',
+  },
+  basic: {
+    adding: '#34C759',
+    destructive: '#FF3B30',
+    black: '#081F32',
+    white: '#FFFFFF',
+    gray: '#C7C7CC',
+  },
+} as const;
